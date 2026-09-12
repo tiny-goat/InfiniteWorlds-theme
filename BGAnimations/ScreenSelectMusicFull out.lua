1fb5249070fa0bab961b2f78b8f2241818fd8f28
tinygoat <euvomediabusiness@gmail.com>
2026-09-12 10:52:17 +0800

    Update to SelectMusic out
    New song confirm sound, simplified banner show animations

--- BGAnimations/ScreenSelectMusicFull out.lua
+++ BGAnimations/ScreenSelectMusicFull out.lua
@@ -2,22 +2,22 @@ return Def.ActorFrame {
     OnCommand=function(self)
 	-- close enough to kpump ig
         self:GetChild("Flash"):diffusealpha(0)
-        self:GetChild("Background"):diffusealpha(0)
-	self:GetChild("Banner"):diffusealpha(0)
-	self:GetChild("Fade"):diffusealpha(0)
+		self:GetChild("ChartStats"):diffusealpha(0)
+		self:GetChild("Banner"):diffusealpha(0)
+		self:GetChild("Fade"):diffusealpha(0)
     end,
     
     StartTransitioningCommand=function(self)
         if SCREENMAN:GetTopScreen():GetNextScreenName() == "ScreenStageInformation" then
-            self:GetChild("Flash"):zoomy(0.3):diffusealpha(1):easeoutexpo(0.3):zoomy(SCREEN_HEIGHT):sleep(1)
+	    self:GetChild("ChartStats"):diffusealpha(1)
 
-	    self:GetChild("Background"):easeoutexpo(0.3):diffusealpha(1)
+	    self:GetChild("Banner"):diffusealpha(1)
 
-	    self:GetChild("Banner"):sleep(0.1):diffusealpha(1):y(SCREEN_CENTER_Y):zoomto(SCREEN_WIDTH*3, SCREEN_HEIGHT*3):croptop(1):easeoutexpo(0.9):croptop(0):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT):sleep(0.2):easeinquad(0.2):zoomto(SCREEN_WIDTH*2, SCREEN_HEIGHT*2):rotationz(5):diffusealpha(0):sleep(1.5)
+        self:GetChild("Flash"):diffusealpha(1):sleep(0.14):easeoutexpo(0.7):diffusealpha(0)
+		
+	    self:GetChild("Fade"):sleep(3.5):linear(0.7):diffusealpha(1)
 
-	    self:GetChild("Fade"):sleep(1.4):linear(0.7):diffusealpha(1)
-
-	    self:GetChild("SFX"):sleep(0.2):play()
+	    self:GetChild("SFX"):sleep(0.1):play()
         else
             self:sleep(0)
         end
@@ -31,11 +31,6 @@ return Def.ActorFrame {
     Def.Sound {
         Name="SFX",
         File=THEME:GetPathS("", "stage_warp")
-    },
-
-    Def.Quad {
-        Name="Flash",
-	InitCommand=function(self) self:FullScreen():diffuse(Color.White) end
     },
 
     Def.Sprite {
@@ -60,8 +55,13 @@ return Def.ActorFrame {
         end
     },
 
+    Def.Quad {
+        Name="Flash",
+		InitCommand=function(self) self:FullScreen():diffuse(Color.White) end
+    },
+
     LoadActor("SongTransition") .. {
-        Name="Background"
+        Name="ChartStats"
     },
 
     Def.Quad {
